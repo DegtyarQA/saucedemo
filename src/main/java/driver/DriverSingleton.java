68f6616d6cbb2f306ac67ca1c58ec4d5819a292d
vdegtjar <v.degtjar@cft.ru>
2023-08-31 18:16:08 +0300

    add classes

--- src/main/java/driver/DriverSingleton.java
+++ src/main/java/driver/DriverSingleton.java
@@ -12,35 +12,29 @@ public class DriverSingleton {
 
     private static WebDriver driver;
 
+    private static ThreadLocal<DriverSingleton> instance = new ThreadLocal<>();
+
     private DriverSingleton() {
+        driver = WebDriverFactory.getWebDriver();
     }
 
-    public static WebDriver getDriver() {
-        if (null == driver) {
-            switch (System.setProperty("browse", "chrome")) {
-                case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                }
-                case "opera": {
-                    WebDriverManager.operadriver().setup();
-                    driver = new OperaDriver();
-                    break;
-                }
-                default: {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                }
-            }
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+    public static synchronized DriverSingleton getInstance() {
+        if (null == instance.get()) {
+            instance.set(new DriverSingleton());
         }
-    return driver;
+        return instance.get();
     }
 
-    public  static void closeDriver(){
-        driver.quit();
-        driver = null;
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void closeDriver() {
+        try {
+            driver.quit();
+            driver = null;
+        } finally {
+            instance.remove();
+        }
     }
 }
